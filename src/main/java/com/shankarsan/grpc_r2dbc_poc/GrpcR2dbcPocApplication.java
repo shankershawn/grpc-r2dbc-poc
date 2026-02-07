7a61eca3389a4d46cf4c851d9a353f2b56b450c8
Shankarsan Ganai <shankershawn@gmail.com>
2026-02-07 21:12:22 +0530

    Initial commit

---
+++ src/main/java/com/shankarsan/grpc_r2dbc_poc/GrpcR2dbcPocApplication.java
@@ -0,0 +1,13 @@
+package com.shankarsan.grpc_r2dbc_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcR2dbcPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcR2dbcPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shankarsan/grpc_r2dbc_poc/GrpcR2dbcPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shankarsan.grpc_r2dbc_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcR2dbcPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
